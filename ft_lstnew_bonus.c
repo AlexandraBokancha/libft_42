18f589bf6255acf51200ecca5b4022852286e5c6
Alexandra Bokancha <125573798+AlexandraBokancha@users.noreply.github.com>
2024-02-22 15:35:19 +0100

    Update ft_lstnew_bonus.c

--- ft_lstnew_bonus.c
+++ ft_lstnew_bonus.c
@@ -10,6 +10,11 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/*Allocates (with malloc(3)) and returns a new node.
+The member variable ’content’ is initialized with
+the value of the parameter ’content’. The variable
+’next’ is initialized to NULL */
+
 #include "libft.h"
 
 t_list	*ft_lstnew(void *content)
@@ -24,13 +29,3 @@ t_list	*ft_lstnew(void *content)
 	return (new);
 }
 
-/* 
-int main()
-{
-    t_list *new;
-    char *str;
-
-    str = "za chto mne vse eto";
-    new = ft_lstnew(str);
-    printf("%s", new->content);
-} */
