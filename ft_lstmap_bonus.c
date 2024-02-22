55da1a20a06512d66fac831c8d95c1aeec17a927
Alexandra Bokancha <125573798+AlexandraBokancha@users.noreply.github.com>
2024-02-22 15:34:23 +0100

    Update ft_lstmap_bonus.c

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -10,7 +10,13 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+/*Iterates the list ’lst’ and applies the function
+’f’ on the content of each node. Creates a new
+list resulting of the successive applications of
+the function ’f’. The ’del’ function is used to
+delete the content of a node if needed */
+
+#include "libft.h" 
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
