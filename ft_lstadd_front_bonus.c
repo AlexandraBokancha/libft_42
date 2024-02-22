20b05a85e951213e5e38f27b2179fd06f882a7b7
Alexandra Bokancha <125573798+AlexandraBokancha@users.noreply.github.com>
2024-02-22 15:30:58 +0100

    Update ft_lstadd_front_bonus.c

--- ft_lstadd_front_bonus.c
+++ ft_lstadd_front_bonus.c
@@ -19,16 +19,3 @@ void	ft_lstadd_front(t_list **lst, t_list *new)
 	new->next = *lst;
 	*lst = new;
 }
-/* 
-int main()
-{
-    char    str2[] = "This is head";
-    char    str[] = "This is new";
-    t_list *new = ft_lstnew(str);
-    t_list *old = ft_lstnew (str2);
-    t_list  *head;
-    head = old;
-    printf("Head == %s\n", (char *)head->content);
-    ft_lstadd_front(&head, new);
-    printf("Head is now == %s\n", (char *)head->content);
-} */
